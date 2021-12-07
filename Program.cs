a21944a504ae98eac4033279c320c26642f85361
Shikorise <logvinenkomihail2711@gmail.com>
2021-12-07 20:26:45 +0700

    Минус

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Lab4._2
         static void Main(string[] args)
         {
             
+                Console.WriteLine("Введите A:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите B:");
+                double b = Convert.ToDouble(Console.ReadLine());
+            }
+            static double min(double A, double B)
+            {
+                return A - B;
+            }
         }
     }
-}
+
